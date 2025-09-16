0a7bf1fd8b845ca1597cb00ad10a2d729991ffb5
Guess2007 <enzogamer70yt@gmail.com>
2025-09-16 10:06:40 -0500

    -

---
+++ Actividad 5/Actividad 5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double precio1, precio2, precio3, precio4, precio5, precio6, precio7, precio8, precio9, precio10;
+            int 
+        }
+    }
+}
